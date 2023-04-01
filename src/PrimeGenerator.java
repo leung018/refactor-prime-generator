7124ffa73b0522be609b76ec4f55f8f8ff7f26bd
Leung Cheng <monewckl@hotmail.com.hk>
2023-04-02 02:17:04 +0800

    format by ide

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -1,12 +1,14 @@
 import java.util.Arrays;
 
-/**　This is my attempt to refactor the original version of PrimeGenerator from the book 'Clean Code' written by
+/**
+ * 　This is my attempt to refactor the original version of PrimeGenerator from the book 'Clean Code' written by
  * Uncle Bob in Chapter 4. I have used a different representation, but kept the same implementation,
  * which is the Sieve of Eratosthenes algorithm.
  */
 public class PrimeGenerator {
 
     private static boolean[] primeFlags; // Indicates whether the number represented by index is prime or not.
+
     public static int[] generatePrimes(int maxValue) {
         if (maxValue < 2)
             return new int[0];
@@ -15,6 +17,7 @@ public class PrimeGenerator {
             return getPrimes();
         }
     }
+
     private static void initPrimeFlags(int maxValue) {
         primeFlags = new boolean[maxValue + 1];
         Arrays.fill(primeFlags, true);
@@ -23,11 +26,12 @@ public class PrimeGenerator {
         for (int i = 2; i <= limit; i++)
             if (primeFlags[i])
                 for (int multiple = 2 * i;
-                    multiple < primeFlags.length;
-                    multiple += i)
+                     multiple < primeFlags.length;
+                     multiple += i)
                     primeFlags[multiple] = false;
 
     }
+
     private static int[] getPrimes() {
         int[] result = new int[numberOfPrimes()];
         for (int j = 0, i = 2; i < primeFlags.length; i++)
--- src/PrimeGeneratorTest.java
+++ src/PrimeGeneratorTest.java
@@ -6,21 +6,21 @@ class PrimeGeneratorTest {
 
     @Test
     public void testGeneratePrimes() {
-        int[] expected = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
+        int[] expected = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
         int[] result = PrimeGenerator.generatePrimes(50);
         assertArrayEquals(expected, result);
     }
 
     @Test
     public void testGeneratePrimesWithMaxValueLessThanTwo() {
-        int[] expected = new int[] {};
+        int[] expected = new int[]{};
         int[] result = PrimeGenerator.generatePrimes(1);
         assertArrayEquals(expected, result);
     }
 
     @Test
     public void testGeneratePrimesWithTwoAsMaxValue() {
-        int[] expected = new int[] {2};
+        int[] expected = new int[]{2};
         int[] result = PrimeGenerator.generatePrimes(2);
         assertArrayEquals(expected, result);
     }
